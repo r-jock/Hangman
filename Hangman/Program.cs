18fe84aeecb2b7beba8470436c1f7972599832f4
r-jock <128504586+r-jock@users.noreply.github.com>
2023-03-23 12:33:40 +0100

    Add some Code
    Added a user Input
    and a empty String Variable.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,15 +7,32 @@
             string[] Wordlist = { "Banane", "Auto", "Käsekuchen", "Pflaumenkuchen" };
             string searchedWord = ""; // Variable to save the founded Letters
             string selectedWord = ""; // Variable to save the Random Word
+
             Random ranWord = new Random(); // New Randomgenerator
             int Rword = ranWord.Next(0, Wordlist.Length); // Generates a Random number from the Length of the Array "Wordlist"
+
             selectedWord = Wordlist[Rword].ToLower();
             Console.WriteLine(selectedWord); // For DEBUGGING
+
             for (int i = 0;i < selectedWord.Length; i++) // Iterate through the Selected Word
             {
                 searchedWord = searchedWord + '_'; // replace the Letter of the Word with "_".
             }
             Console.Write(searchedWord); // Printed The Word as '_' on the Console
+
+            Console.WriteLine("Gebe ein Buchstaben ein: ");
+            string Mysuggestion = Console.ReadLine();
+            string myWord = "";
+
+            for (int i; i < searchedWord.Length; i++)
+            {
+                if (Mysuggestion == searchedWord)
+                {
+                    myWord = searchedWord[i] + myWord;
+                }
+            }
+            
+            
             Console.ReadKey();
 
         }
