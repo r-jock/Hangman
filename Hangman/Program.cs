f9af1e41142062153906d009cc43518a2cec83d3
r-jock <128504586+r-jock@users.noreply.github.com>
2023-03-24 16:46:49 +0100

    added a Letter Counter and a Win Condition

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -4,6 +4,7 @@
     {
         static char[] word;
         static bool[] guess;
+        static int misstake;
 
         static void init(string s)
         {
@@ -19,10 +20,43 @@
                 else Console.Write(word[i] + " ");
             }
         }
+
+        static int countLetter(char c)
+        {
+            int count = 0;
+            for(int i = 0;i<word.Length; i++)
+            {
+                if (word[i]== c)
+                {
+                    guess[i] = true;
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        static bool won()
+        {
+            for(int i = 0; i < word.Length; i++)
+            {
+                if (!guess[i])
+                    return false;
+            }
+            return true;
+        }
+
         static void Main(string[] args)
         {
+            
             init("Word");
             getWord();
+            do
+            {
+                char c = Console.ReadKey(true).KeyChar;
+                countLetter(c);
+                getWord();
+
+            } while (!won());
             
             Console.ReadKey();
         }
